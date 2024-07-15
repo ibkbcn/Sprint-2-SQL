f3c060d0c91a2aafea0b16908cfe300cf4945999
IvanBK <140195575+ibkbcn@users.noreply.github.com>
2024-07-15 15:59:54 +0200

    Add files via upload

--- Sprint 2 resumen.sql
+++ Sprint 2 resumen.sql
@@ -1,12 +1,19 @@
+# Nivell 1
+
+# Ejercicio 1
 # Se da por entendido que anteriormente ya se ha ejecutado la estructura de la base de datos y se han insertado los datos en las distintas tablas. (Respectivos al Ejercicio 1) 
 # Tablas company y transaction creadas y con datos.
 
 #Ejercició 2
 
+# Uso el distinct para obtener un listado de los países donde se ubican nuestros clientes, el cual no muestre los duplicados.
 SELECT distinct country FROM company JOIN transaction ON company.id = transaction.company_id;
 
+#Aquí le agrego count como operación de agregación a la query anteriormente realizada. Esta operación me retornara como resultado la cantidad de países distintos a los que pertenecen las empresas con las que trabajamos.
 SELECT count(distinct country) FROM company JOIN transaction ON  company.id = transaction.company_id;
 
+#En la tercera query quiero conocer la empresa con mayor promedio de amount. Para ello calculo el promedio de compra agrupado por compañía, lo ordeno por promedio amount DESC y al final insertamos LIMIT 1.
+#Además, en el WHERE se eligen solo las transacciones que no han sido declinadas.
 SELECT company_name, avg(amount) 
 FROM company 
 JOIN transaction ON company.id = transaction.company_id 
@@ -16,9 +23,10 @@ order by avg(amount) DESC
 LIMIT 1;
 
 #Ejercició 3
-
+#Utilizando subquery le pido que solo me retorne los datos de transacciones correspondientes a las empresas alemanas.
 SELECT id FROM transaction WHERE company_id IN (SELECT id FROM company WHERE country = 'Germany');  
 
+#Realizo query pidiendo el nombre de las compañías que están por encima del promedio en amount, usando el método de subquery de WHERE.
 SELECT company_name
 FROM company
 WHERE id IN (
@@ -30,13 +38,14 @@ WHERE id IN (
     )
 );   
 
+# Busco conocer las empresas que no tienen transacciones, para ello uso la cláusula NOT IN.
 SELECT c.company_name 
 FROM company c 
 WHERE c.id NOT IN (
 	SELECT t.company_id 
     FROM transaction t);
     
-#testeo
+#Testeo para confirmar que no haya ninguna empresa sin transacciones.
 SELECT c.company_name, count(t.id)
 FROM company c 
 JOIN transaction t ON c.id = t.company_id
